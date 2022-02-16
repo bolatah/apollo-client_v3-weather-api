719a9d6d03fab1d2f5b10de8a7fbab26147d5050
bolatah <bolatah@outlook.com>
2022-02-16 23:59:41 +0100

    first commit

---
+++ src/graphql/Queries.js
@@ -0,0 +1,36 @@
+// inside of it we write just graphql queries
+import { gql } from "@apollo/client";
+
+// for every query we write a function and assign to a var
+
+export const GET_WEATHER_QUERY = gql`
+  query getCityByName($name: String!) {
+    getCityByName(name: $name) {
+      name
+      country
+      weather {
+        summary {
+          title
+          description
+          icon
+        }
+        temperature {
+          actual
+          feelsLike
+          min
+          max
+        }
+        wind {
+          speed
+          deg
+        }
+        clouds {
+          all
+          visibility
+          humidity
+        }
+        timestamp
+      }
+    }
+  }
+`;
